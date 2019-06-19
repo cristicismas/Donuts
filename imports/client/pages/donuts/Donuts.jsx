704348f76e4e8bfb6c44ad9005e5ad410d1411d3
Cristi Cismas <cristicismas99@gmail.com>
2019-06-19 11:52:43 +0300

    Add the Header component to the /donuts route

--- imports/client/pages/donuts/Donuts.jsx
+++ imports/client/pages/donuts/Donuts.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 import '../../css/Donuts.css';
 
 import DonutsCreate from './DonutsCreate';
 import DonutsList from './DonutsList';
 import DonutsRecent from './DonutsRecent';
+import Header from '../../components/Header';
 
-class Donuts extends React.Component {
+class Donuts extends Component {
     render() {
         return (
-            <main id="donuts">
-                <div className="donuts-create-container">
-                    <DonutsCreate />
-                </div>
-                <DonutsList />
-                <DonutsRecent />
-            </main>
+            <Fragment>
+                <Header />
+                <main id="donuts">
+                    <div className="donuts-create-container">
+                        <DonutsCreate />
+                    </div>
+                    <DonutsList />
+                    <DonutsRecent />
+                </main>
+            </Fragment>
         );
     }
 }
