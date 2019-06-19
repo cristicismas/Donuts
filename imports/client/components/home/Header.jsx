9186e7a8ec8bf2d2cf6214189c6dbb631b8f19b3
Cristi Cismas <cristicismas99@gmail.com>
2019-06-19 17:35:41 +0300

    Add Login / Register link to the Header if the user is not logged in

--- imports/client/components/home/Header.jsx
+++ imports/client/components/home/Header.jsx
@@ -5,10 +5,22 @@ import Notifications from './Notifications';
 
 class Header extends Component {
     render() {
+        const loggedIn = Meteor.userId() ? true : false;
+
         return (
             <header>
                 <a href="/"><h1>donut</h1></a>
-                <Notifications />
+
+                {
+                    loggedIn ?
+                    (
+                        <nav>
+                            <Notifications />
+                        </nav>
+                    ) : (
+                        <a href="/login"><h3>Login / Register</h3></a>
+                    )
+                }
             </header>
         )
     }
